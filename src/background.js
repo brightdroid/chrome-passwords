a4a4ad6807a8f1b06bd71e5eae4c29989084f60c
Christoph Roeder <phpmyforum@gmail.com>
2014-11-10 14:51:50 +0100

    fix: username was not used

--- src/background.js
+++ src/background.js
@@ -13,36 +13,39 @@ chrome.runtime.onMessage.addListener(function(msg, sender)
 	 */
     else if (msg.action == "generate")
 	{
-		var mpw = new MPW("chris", msg.master);
-		
-		var hasher = mpw.generatePassword(msg.domain, msg.counter, msg.template);
-		
-		hasher.then(
-			function (pass)
-			{
-				chrome.tabs.query(
-					{
-						active: true,
-						currentWindow: true
-					},
-					function(tabs)
-					{
-						chrome.tabs.sendMessage(
-							tabs[0].id,
-							{
-								from: "background",
-								action: "setPassword",
-								password: pass
-							}
-						);
-					}
-				);
+		chrome.storage.sync.get("opt:username", function(prefs)
+		{
+			var mpw = new MPW(prefs["opt:username"], msg.master);
+			
+			var hasher = mpw.generatePassword(msg.domain, msg.counter, msg.template);
+			
+			hasher.then(
+				function (pass)
+				{
+					chrome.tabs.query(
+						{
+							active: true,
+							currentWindow: true
+						},
+						function(tabs)
+						{
+							chrome.tabs.sendMessage(
+								tabs[0].id,
+								{
+									from: "background",
+									action: "setPassword",
+									password: pass
+								}
+							);
+						}
+					);
 
-			},
-			function (err)
-			{
+				},
+				function (err)
+				{
 
-			}
-		);
+				}
+			);
+		});
 	}
 });
