eaffbfd9aaf6ba1a3a23fa7d1ebedf28e2969d46
Christoph Roeder <phpmyforum@gmail.com>
2014-11-10 17:19:41 +0100

    added dblclick event handler to password fields
    This enables the selection of only one field for e.g. password change forms. So one can choose not to fill the "old" password field with the new password.

--- src/content.js
+++ src/content.js
@@ -11,24 +11,68 @@
 
 
 /**
+ * password field selection
+ */
+function toggleMPW(e)
+{
+	elm = e.target;
+	val = elm.getAttribute("data-mpw");
+	
+	// disable
+	if (val && val == "true")
+	{
+		elm.style.backgroundPositionX = "-100px";
+		elm.setAttribute("data-mpw", false);
+		
+	}
+	// enable
+	else
+	{
+		elm.style.backgroundImage = "url(" + chrome.extension.getURL("res/icon38.png") + ")";
+		elm.style.backgroundPosition = "100% 50%";
+		elm.style.backgroundRepeat = "no-repeat";
+		elm.style.backgroundSize = "contain";
+		elm.setAttribute("data-mpw", true);
+	}
+}
+
+
+/**
+ * add event handler
+ */
+var fields = document.querySelectorAll("input[type=password]");
+for (i = 0; i < fields.length; i++)
+{
+	fields[i].addEventListener("dblclick", toggleMPW);
+}
+
+
+/**
  * extension messaging
  */
 chrome.runtime.onMessage.addListener(function(msg, sender, response)
 {
-    if (msg.from == "background" && msg.action === "setPassword")
+	if (msg.from == "background" && msg.action === "setPassword")
 	{
-		// update password fields
-		var fields = document.querySelectorAll("input[type=password]")
-
+		// are there selected fields?
+		fields = document.querySelectorAll("input[type=password][data-mpw=true]");
+		
+		// no selected fields found, find all!
+		if (fields.length == 0)
+		{
+			fields = document.querySelectorAll("input[type=password]")
+		}
+		
+		// update values
 		for (i=0; i < fields.length; i++)
 		{
 			fields[i].value = msg.password;
 		}
-
+		
 		// close popup
 		chrome.runtime.sendMessage({
 			from: "content",
 			action: "closePopup"
 		});
-    }
+	}
 });
